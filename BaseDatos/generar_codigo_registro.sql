81ecab7910877d4d909718375c5b07aa2bb28407
Juan Cabuyales <juan.cabuyales@supergiros.com.co>
2020-07-07 12:32:28 -0500

    Se crea procedimiento para generar codigo de registro y se agrega utilidad para llamar procedimientos @Author: Juan Cabuyales

---
+++ BaseDatos/generar_codigo_registro.sql
@@ -0,0 +1,47 @@
+CREATE OR REPLACE PROCEDURE prdb_generar_codigo_registro(IN p_celular usuario.usuario_celular%TYPE, INOUT p_resultado character varying)
+LANGUAGE plpgsql    
+AS $$
+DECLARE
+	cant_intentos numeric;
+	codigo_generado character varying;
+	
+	v_state   text;
+    v_msg     text;
+    v_detail  text;
+    v_hint    text;
+    v_context text;
+    	
+BEGIN
+	
+	SELECT (pa.parametro_valor::numeric)
+	INTO cant_intentos
+	FROM parametro pa
+	WHERE pa.parametro_nombre = 'CANT_INT_COD_REGISTRO';
+	
+	codigo_generado := generar_aleatorios(111111,999999)::character varying;
+	
+	INSERT INTO codigo (codigo_numero,
+						codigo_celular,
+						codigo_intentos,
+						codigo_fecha_hora_registro,
+						codigo_estado) 
+				VALUES (codigo_generado,
+						p_celular,
+						cant_intentos,
+						NOW(),
+						1);
+	p_resultado := '0,' || codigo_numero ||','||codigo_celular;
+	
+EXCEPTION WHEN OTHERS THEN
+
+	GET stacked DIAGNOSTICS
+        v_state   = returned_sqlstate,
+        v_msg     = message_text,
+        v_detail  = pg_exception_detail,
+        v_hint    = pg_exception_hint,
+        v_context = pg_exception_context;
+        
+	p_resultado:= '1,Se ha presentado un error inesperado: '||v_state||' '||v_msg; 	
+	
+END;
+$$;
--- BaseDatos/utilidades.sql
+++ BaseDatos/utilidades.sql
@@ -2,3 +2,13 @@ INSERT INTO usuario(usuario_nombre,usuario_fecha_hora_acepta_terminos,usuario_co
 VALUES ('Juan Fernando',NOW(),'juancabuyales@gmail.com','3147452427','Cabuyales','S');
 
 ALTER SEQUENCE woaho.sec_usuario RESTART WITH 1;
+
+/** Bloque anonimo que permite ejecutar un procedimiento**/
+DO $$ 
+<<first_block>>
+DECLARE
+  salida character varying;
+BEGIN 
+   call woaho.prdb_generar_codigo_registro('3147453245',salida);
+   RAISE NOTICE 'esto salio : %', salida;
+END first_block $$;
