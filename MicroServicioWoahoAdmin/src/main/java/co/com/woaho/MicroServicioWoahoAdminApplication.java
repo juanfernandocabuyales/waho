0474751c1a146891bad033a106b1ef859a65e15f
Juan <jfbeca@latam.stefanini.com>
2021-01-20 18:23:04 -0500

    Se crea microservicio para el portal administrativo @Author: Juan Cabuyales

---
+++ MicroServicioWoahoAdmin/src/main/java/co/com/woaho/MicroServicioWoahoAdminApplication.java
@@ -0,0 +1,22 @@
+package co.com.woaho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+import co.com.woaho.utilidades.RegistrarLog;
+
+@SpringBootApplication
+@ComponentScan(basePackages = { "co.com.woaho"})
+@EntityScan("co.com.woaho")
+public class MicroServicioWoahoAdminApplication {
+	
+	private static RegistrarLog logs = new RegistrarLog(MicroServicioWoahoAdminApplication.class);
+
+	public static void main(String[] args) {
+		logs.registrarInfo("INICIA SERVICIO ADMIN 20/01/2021 18:15 pm");
+		SpringApplication.run(MicroServicioWoahoAdminApplication.class, args);
+	}
+
+}
---
+++ MicroServicioWoahoAdmin/src/main/java/co/com/woaho/utilidades/RegistrarLog.java
@@ -0,0 +1,101 @@
+package co.com.woaho.utilidades;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class RegistrarLog {
+
+	private Logger logger;
+	
+	private static final String HILO = "Hilo[";
+	private static final String CIERRE_HILO = "] ";
+	private static final String INFO_METODO = "Se va a ejecutar el metodo: ";
+	private static final String INFO_PARAMETROS = "con parametros: ";
+	private static final String INFO_QUERY = "se ejecuta el query: ";
+	private static final String ERROR_METODO = "se ha presentado un error en el metodo: ";
+	private static final String INFO_PAQFUNCION = "Se va a ejecutar la funcion y/o procedimiento: ";
+	private static final String INFO_RESULTADO = "resultado obtenido: ";
+	private static final String INFO_SERVICIO = "Se va a ejecutar el servicio: ";
+	private static final String RESPONSE_SERVICIO = "Response del servicio: ";
+	
+	public RegistrarLog() {
+		super();
+	}
+	
+	public RegistrarLog(Class<?> classlog) {
+		super();
+		logger = LoggerFactory.getLogger(classlog);
+	}
+	
+	public void registrarInfo(String pInformacion) {
+		logger.info(HILO + Thread.currentThread().getId() + CIERRE_HILO + pInformacion);
+	}
+	
+	public void registrarInfo(Object pObjecto) {
+		logger.info(HILO + Thread.currentThread().getId() + CIERRE_HILO + pObjecto);
+	}
+	
+	public void registrarDebug(Object pObjecto) {
+		logger.debug("result : {}", pObjecto);
+	}
+	
+	public void registrarError(Exception e) {
+		logger.error(HILO + Thread.currentThread().getId() + CIERRE_HILO + e.getMessage(),e);
+	}
+	
+	public void registrarError(Exception e,String pMensaje) {
+		logger.error(HILO + Thread.currentThread().getId() + CIERRE_HILO + pMensaje,e);
+	}
+	
+	public void registrarLogInfoEjecutaMetodo(String pNombreMetodo) {
+		logger.info(HILO + Thread.currentThread().getId() + CIERRE_HILO + INFO_METODO + pNombreMetodo);
+	}
+	
+	public void registrarLogInfoEjecutaMetodoConParam(String pNombreMetodo, String pParametros) {
+		logger.info(HILO + Thread.currentThread().getId() + CIERRE_HILO + INFO_METODO + pNombreMetodo + INFO_PARAMETROS
+				+ pParametros);
+	}
+	
+	public void registrarLogInfoEjecutaQuery(String pNombreQuery) {
+		logger.info(HILO + Thread.currentThread().getId() + CIERRE_HILO + INFO_QUERY + pNombreQuery);
+	}
+	
+	public void registrarLogInfoEjecutaQueryConParam(String pNombreQuery, String pParametros) {
+		logger.info(HILO + Thread.currentThread().getId() + CIERRE_HILO + INFO_QUERY + pNombreQuery + INFO_PARAMETROS
+				+ pParametros); 
+	}
+	
+	public void registrarLogError(String pNombreMetodo, String pMensaje,Exception e) {
+		logger.error(HILO + Thread.currentThread().getId() + CIERRE_HILO + ERROR_METODO + pNombreMetodo + " " + pMensaje,e);
+	}
+	
+	public void registrarLogInfoEjecutaPaqFuncConParam(String pNombreFuncion, String pParametros) {
+		logger.info(HILO + Thread.currentThread().getId() + CIERRE_HILO + INFO_PAQFUNCION + pNombreFuncion + INFO_PARAMETROS
+				+ pParametros);
+	}
+	
+	public void registrarLogInfoEjecutaPaqFunc(String pNombreFuncion) {
+		logger.info(HILO + Thread.currentThread().getId() + CIERRE_HILO + INFO_PAQFUNCION + pNombreFuncion);
+	}
+	
+	public void registrarLogInfoResultado(String pMensaje) {
+		logger.info(HILO  + Thread.currentThread().getId() + CIERRE_HILO + INFO_RESULTADO + pMensaje);
+	}
+	
+	public void registrarLogInfoEjecutaServicio(String pNombreServicio) {
+		logger.info(HILO  + Thread.currentThread().getId() + CIERRE_HILO + INFO_SERVICIO + pNombreServicio);
+	}
+	
+	public void registrarLogInfo(String pMensaje) {
+		logger.info(HILO  + Thread.currentThread().getId() + CIERRE_HILO + pMensaje);
+	}
+	
+	public void registrarLogInfoRespuestaServicio(String pNombreServicio,String pResponse) {
+		logger.info(HILO  + Thread.currentThread().getId() + CIERRE_HILO + RESPONSE_SERVICIO + pNombreServicio + " Response: " + pResponse);
+	}
+	
+	public void registrarLogInfoEjecutaServicio(String pNombreServicio,String pPeticion) {
+		logger.info(HILO  + Thread.currentThread().getId() + CIERRE_HILO + INFO_SERVICIO + pNombreServicio + " Peticion: " + pPeticion);
+	}
+}
